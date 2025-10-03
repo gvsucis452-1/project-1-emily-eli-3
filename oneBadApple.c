32f1833bd5b84eaab13a6c311209925230fb91db
emheyboer <157163774+emheyboer@users.noreply.github.com>
2025-10-03 14:25:34 -0400

    Make node 0 an actual node and synchronize it with the others

--- oneBadApple.c
+++ oneBadApple.c
@@ -20,10 +20,6 @@ byte  |     definition
 0-3   |     INT - Recipient Node ID
 4-1024|     PTR - Message ptr
 ==========================================================*/
-/*NOTE - Special Messages =================================
-[ID] | C^   - Graceful shutdown of ring. Not implemented yet.
-[ID] | PING - Ring health check, should reach HEAD. Not implemented yet, optional.
-==========================================================*/
 pid_t nextNode;
 int nodeId;
 struct Message *msg;
@@ -44,8 +40,8 @@ void shutdown() {
     exit(0);
 }
 
-int msgLoop(const int PREV_READ_PIPE, const int NEXT_WRITE_PIPE) {
-    struct Message *msg = malloc(sizeof(struct Message));
+void msgLoop(const int PREV_READ_PIPE, const int NEXT_WRITE_PIPE) {
+    msg = malloc(sizeof(struct Message));
     ssize_t msgLen; //NOTE Remove this if we start using Message->length
     const pid_t pid = getpid();
 
@@ -53,19 +49,18 @@ int msgLoop(const int PREV_READ_PIPE, const int NEXT_WRITE_PIPE) {
 
     if (msg == NULL) {
         perror("Malloc failed!\n");
-        return 1;
+        shutdown();
     }
 
     while (1)
     {
         msgLen = read(PREV_READ_PIPE, msg, MAX_MSG_LEN);
         if (msgLen == -1) {
-            // TODO: Differentiate between dead nodes and gracefully shutting down the ring.
             perror("Read failed, previous node dead or detached, exiting...\n");
             shutdown();
         }
         
-        if (msgLen < sizeof(int) || msg->recipientId < 1 || msg->recipientId > MAX_NODES) {
+        if (msgLen < sizeof(int) || msg->recipientId < 0 || msg->recipientId > MAX_NODES) {
             printf("Node %d (PID: %d): Malformed or partial message, ignoring...\n", nodeId, pid);
             continue;
         }
@@ -73,13 +68,69 @@ int msgLoop(const int PREV_READ_PIPE, const int NEXT_WRITE_PIPE) {
         // Check if msg is for this node
         if (nodeId == msg->recipientId) {
             printf("Message at Node %d (PID: %d) received: %s\n", nodeId, pid, msg->content);
-        } else {
-            printf("Node %d (PID: %d) passing message to node %d.\n", nodeId, pid, msg->recipientId);
-            // TODO: Forward message to next node in ring
-            write(NEXT_WRITE_PIPE, msg, MAX_MSG_LEN);
+            msg->recipientId = 0;
         }
+
+        // we always pass the message on to the next node (even if it's destined for this one)
+        printf("Node %d (PID: %d) passing message addressed to node %d.\n", nodeId, pid, msg->recipientId);
+        write(NEXT_WRITE_PIPE, msg, MAX_MSG_LEN);
     };
 };
+
+void inputLoop(const int PREV_READ_PIPE, const int NEXT_WRITE_PIPE, int k_nodes) {
+    struct Message *msg = malloc(sizeof(struct Message));
+
+    if (msg == NULL) {
+        perror("Malloc failed!\n");
+        exit(1);
+    }
+
+    char *buffer = malloc(sizeof(struct Message));
+
+    if (buffer == NULL) {
+        perror("Malloc failed!\n");
+        exit(1);
+    }
+
+    signal(SIGINT, shutdown);
+
+    // after initializing the node ring,
+    // we send a message and wait for it to come back
+    // so we know every node has been initialized
+    msg->recipientId = 0;
+    write(NEXT_WRITE_PIPE, msg, MAX_MSG_LEN);
+    read(PREV_READ_PIPE, msg, MAX_MSG_LEN);
+    
+    while (1) {
+        printf("Enter message as [ID][MSG]:\n>");
+        if (fgets(buffer, MAX_MSG_LEN, stdin) == NULL) {
+            printf("Failed buffer write. Exiting.\n");
+            break;
+        }
+
+        switch (sscanf(buffer, "%d %[^\n]", &msg->recipientId, msg->content))
+        {
+        case 0:
+            printf("Invalid message, use [ID][MSG]\n");
+            continue;
+        case 1:
+            printf("Please include both [ID] & [MSG]\n");
+            continue;
+        default:
+            break;
+        }
+        
+        if (msg->recipientId < 1 || msg->recipientId > k_nodes) {
+            printf("Bad ID value, please choose a value between 1-%d\n", k_nodes);
+            continue;
+        }
+        
+        write(NEXT_WRITE_PIPE, msg, MAX_MSG_LEN);
+        read(PREV_READ_PIPE, msg, MAX_MSG_LEN);
+    }
+
+    free(buffer);
+}
 
 void initNode(int k, int id, const int PREV_READ_PIPE, const int HEAD_WRITER) {
     // Init new pipe
@@ -101,6 +152,7 @@ void initNode(int k, int id, const int PREV_READ_PIPE, const int HEAD_WRITER) {
     // Else Create N(id + 1)
     } else {
         nextNode = fork();
+
         if (nextNode == -1) {
             perror("Fork failed!\n");
             exit(1);
@@ -108,16 +160,17 @@ void initNode(int k, int id, const int PREV_READ_PIPE, const int HEAD_WRITER) {
             // Child recursively starts as next node
             initNode(k, id + 1, nodePipe[0], HEAD_WRITER);
         }
-        // Parent continues
-        msgLoop(PREV_READ_PIPE, nodePipe[1]);
+        
+        if (nodeId) {
+            msgLoop(PREV_READ_PIPE, nodePipe[1]);
+        } else { // node 0 handles user input
+            inputLoop(PREV_READ_PIPE, nodePipe[1], k);
+        }
     }
-    // Cleanup
-    close(PREV_READ_PIPE);
-    exit(0);
 };
 
 
-int initRing(int k) {
+void initRing(int k) {
     // Init pipe
     int nodePipe[2];
     if (pipe(nodePipe) == -1) {
@@ -126,16 +179,8 @@ int initRing(int k) {
     }
     const int HEAD_READER = nodePipe[0];
     const int HEAD_WRITER = nodePipe[1];
-    
-    nextNode = fork();
-    if (nextNode == -1) {
-        perror("Fork failed!\n");
-        exit(1);
-    } else if (nextNode == 0) {
-        initNode(k, 1, HEAD_READER, HEAD_WRITER); // id = 1 since this is the 2nd Node, not 1st
-    }
-    // Parent continues
-    return nodePipe[1];
+
+    initNode(k, 0, HEAD_READER, HEAD_WRITER);
 };
 
 int main(int argc, char *argv[]) {
@@ -143,56 +188,13 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: ./oneBadApple <num_nodes>\n");
         exit(1);
     }
+
     // Convert string argument to integer for number of nodes
     const int nodes = atoi(argv[1]);
     if (nodes < 1 || nodes > MAX_NODES) {
         perror("Error: Number of nodes must be 1-1000\n");
         exit(1);
     }
-    const int HEAD_PIPE = initRing(nodes);
-    struct Message *msg = malloc(sizeof(struct Message));
 
-    if (msg == NULL) {
-        perror("Malloc failed!\n");
-        exit(1);
-    }
-
-    char *buffer = malloc(sizeof(struct Message));
-
-    if (buffer == NULL) {
-        perror("Malloc failed!\n");
-        exit(1);
-    }
-
-    signal(SIGINT, shutdown);
-    
-    while (1) {
-        printf("Enter message as [ID][MSG]:\n>");
-        if (fgets(buffer, MAX_MSG_LEN, stdin) == NULL) {
-            printf("Failed buffer write. Exiting.\n");
-            break;
-        }
-
-        switch (sscanf(buffer, "%d %[^\n]", &msg->recipientId, msg->content))
-        {
-        case 0:
-            printf("Invalid message, use [ID][MSG]\n");
-            continue;
-        case 1:
-            printf("Please include both [ID] & [MSG]\n");
-            continue;
-        default:
-            break;
-        }
-        
-        if (msg->recipientId < 1 || msg->recipientId > nodes) {
-            printf("Bad ID value, please choose a value between 1-%d\n", nodes);
-            continue;
-        }
-        
-        write(HEAD_PIPE, msg, MAX_MSG_LEN);
-    }
-    free(msg);
-    free(buffer);
-    return 0;
+    initRing(nodes);
 }
